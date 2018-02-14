f45cdd973b79eb757b2e9dfa0ca28b51c88f4a5e
Stephen Newman <steve@kodesh.io>
2018-02-14 14:24:46 +0000

    Force a road into the wrong partition.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Contracts;
+using Microsoft.ServiceFabric.Services.Client;
 using Microsoft.ServiceFabric.Services.Remoting.Client;
 using Microsoft.ServiceFabric.Services.Remoting.V2.FabricTransport.Client;
 
@@ -9,9 +10,11 @@ namespace Client
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Press a key once the cluster is ready.");
             Console.ReadKey(true);
 
             AddRoad("London", "Downing Street");
+            AddRoadToWrongParition("London", "Bond Street");
             AddRoad("London", "Oxford Street");
             AddRoad("York", "Micklegate");
 
@@ -41,6 +44,20 @@ namespace Client
         private static void DescribeServiceInteraction(ServiceInteraction serviceInteraction)
         {
             Console.WriteLine($"{serviceInteraction.PartitionId} was enlisted at {serviceInteraction.InteractedAtUtc.ToShortDateString()} {serviceInteraction.InteractedAtUtc.ToShortTimeString()}");
+            Console.WriteLine();
+        }
+
+        private static ServiceInteraction AddRoadToWrongParition(string town, string roadName)
+        {
+            Console.WriteLine($"Adding road: {roadName} to town: {town} but with the wrong partition strategy.");
+            var serviceUri = new Uri($"fabric:/ServiceFabricApplication/RoadService");
+            var proxyFactory = new ServiceProxyFactory(_ => new FabricTransportServiceRemotingClientFactory());
+            var partitionKey = new ServicePartitionKey(1848674407370955162);
+            var roadService = proxyFactory.CreateServiceProxy<IRoadService>(serviceUri, partitionKey);
+            var serviceInteraction = roadService.AddRoad(town, roadName).GetAwaiter().GetResult();
+            DescribeServiceInteraction(serviceInteraction);
+            return serviceInteraction;
+
         }
 
         private static ServiceInteraction ShowRoads(string town)
